d97585d43b50b8f16375ae991e5992a1c99943ef
rooshmintted <roosh@mintted.io>
2025-07-18 16:36:32 -0500

    Updated analytics for vercel

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,29 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    const isDev = process.env.NODE_ENV === 'development'
+    
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              `script-src 'self' 'unsafe-inline' ${isDev ? "'unsafe-eval'" : ''} https://va.vercel-scripts.com`,
+              "connect-src 'self' https://vitals.vercel-insights.com https://*.supabase.co wss://*.supabase.co",
+              "img-src 'self' data: https:",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+              "font-src 'self' http://fonts.gstatic.com",
+              "style-src-elem 'self' 'unsafe-inline' http://fonts.googleapis.com"
+            ].join('; ')
+          }
+        ]
+      }
+    ]
+  }
 }
 
 export default nextConfig
